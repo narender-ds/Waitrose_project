5d9d2208b683590a1d2eee85214bcd9931e34c20
narender-ds <narender.singh@dotsquares.com>
2023-03-01 12:56:02 +0530

    first Deploy

---
+++ src/components/layouts/footer.tsx
@@ -0,0 +1,165 @@
+import * as React from "react";
+type DataOfProps = {
+  c_footerLogo: any;
+  c_waitroseHelpAndSupportSection: any;
+  c_usefulInformationSection: any;
+  c_moreFromUsSection: any;
+  c_aboutWaitroseSection: any;
+  c_socialIcon: any;
+  c_visitOurMobileSiteTitle:any;
+  c_footerImage:any;
+  c_footerLogoText:any;
+  c_copyrightTag:any;
+};
+const Footer = (props: DataOfProps) => {
+  const {
+    c_footerLogo,
+    c_waitroseHelpAndSupportSection,
+    c_usefulInformationSection,
+    c_moreFromUsSection,
+    c_aboutWaitroseSection,
+    c_socialIcon,
+    c_visitOurMobileSiteTitle,
+    c_footerImage,
+    c_footerLogoText,
+    c_copyrightTag
+  } = props;
+  console.log("c_visitOurMobileSiteTitle.followTitle", c_visitOurMobileSiteTitle);
+  return (
+    <footer
+      className="inline-block w-full
+    bg-[#F9F9F9]  text-white"
+    >
+             <h2 className="footerLogoText">{c_footerLogoText}</h2>
+      <div className="container px-6 pt-6 flex">
+        {/* <img src={c_footerLogo.url} alt="" /> */}
+ 
+        <div className="flex">
+          <div className=" grid lg:grid-cols-4 md:grid-cols-2">
+            <div className="Footer-navList">
+              <h4 style={{ fontWeight: "bold", color: "black" }}>
+                {c_waitroseHelpAndSupportSection.title}
+              </h4>
+              {c_waitroseHelpAndSupportSection?.labelsIcons?.map(
+                (item: any) => {
+                  return (
+                    <>
+                      <ul>
+                        <li>
+                          <div>
+                            <a className="" href="#">
+                              {item.navigationLinks.label}
+                              <img src={item.navigationIcon.url} className="inline"/>
+                            </a>
+                          </div>
+                        </li>
+                      </ul>
+                    </>
+                  );
+                }
+              )}
+            </div>
+            <div className="Footer-navList">
+              <h4 style={{ fontWeight: "bold", color: "black" }}>
+                {" "}
+                {c_usefulInformationSection.title}
+              </h4>
+              {c_usefulInformationSection?.labelsIcons?.map((item: any) => {
+                return (
+                  <>
+                    <ul>
+                      <li>
+                        <div>
+                          <a className="" href="#">
+                            {item.navigationLinks.label}
+                            <img src={item.navigationIcon.url} className="inline"/>
+                          </a>
+                        </div>
+                      </li>
+                    </ul>
+                  </>
+                );
+              })}
+            </div>
+            <div className="Footer-navList">
+              <h4 style={{ fontWeight: "bold", color: "black" }}>
+                {c_moreFromUsSection.title}
+              </h4>
+              {c_moreFromUsSection?.labelsIcons?.map((item: any) => {
+                return (
+                  <>
+                    <ul>
+                      <li>
+                        <div>
+                          <a className="" href="#">
+                            {item.navigationLinks.label}
+                            <img src={item.navigationIcon.url} className="inline"/>
+                          </a>
+                        </div>
+                      </li>
+                    </ul>
+                  </>
+                );
+              })}
+            </div>
+            <div className="Footer-navList">
+              <h4 style={{ fontWeight: "bold", color: "black" }}>
+                {c_aboutWaitroseSection.title}
+              </h4>
+              {c_aboutWaitroseSection?.labelsIcons?.map((item: any) => {
+                return (
+                  <>
+                    <ul>
+                      <li>
+                        <div>
+                          <a className="" href="#">
+                            {item.navigationLinks.label}
+                            <img src={item.navigationIcon.url} className="inline"/>
+                           
+                          </a>
+                        </div>
+                      </li>
+                    </ul>
+                  </>
+                );
+              })}
+            </div>
+                  </div>
+        
+          <div className="socialIcondiv">
+            <p className="socialIconp"> {c_socialIcon.followTitle}</p>
+            <ul className="socialiconUl ">
+              <li >
+                <a
+                  data-cy="social-links"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.twitter.com/papajohnsmx"
+                  aria-label="Visit our Twitter page"
+                >
+                  <span aria-hidden="true">
+                    {c_socialIcon.iconImage.map((item: any) => {
+                      return (
+                        <>
+                          <img className="socialIcons" src={item.url} />
+                        </>
+                      );
+                    })}{" "}
+                  </span>
+                </a>
+              </li>
+            </ul>
+            <p className="socialIconp">{c_visitOurMobileSiteTitle}</p>
+          </div>
+        </div>
+        <div>
+        <img src={c_footerImage.url} alt="" />
+        </div>
+      </div>
+   <p className="copyright">{c_copyrightTag}</p> 
+      {/* </div > */}
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/layouts/header.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import Cta from "../commons/cta";
+
+type DataOfProps = {
+  c_headerNav: any;
+  c_bookASlotCta: any;
+  c_headerMain: any;
+  c_myTrolleyCta: any;
+  // c_bookASlotCta: any;
+};
+
+const Header = (props: DataOfProps) => {
+  const { c_headerNav, c_bookASlotCta, c_headerMain, c_myTrolleyCta } = props;
+  console.log("c_bookASlotCta", c_bookASlotCta);
+
+  return (
+    <>
+      <div className="inline-block w-full  text-#777">
+        <div className="container px-6 pt-6">
+          <nav className="py-6 flex items-center justify-between">
+            {c_headerNav.headerExploreTtile}
+            <div className="flex gap-x-10 text-lg font-semibold">
+              {" "}
+              {c_headerNav.headerNavigationLinks.map((itemLabels: any) => {
+                return (
+                  <>
+                    <a href="">{itemLabels.label}</a>
+                  </>
+                );
+              })}
+            </div>
+            <div className="space-x-5">
+              <a href=""> {c_headerNav.headerMyAccount.label}</a>
+              <a href="">{c_headerNav.headerHelp.label}</a>
+              <a href=""> {c_headerNav.headerMyWaitrose.label}</a>
+              <a href=""> {c_headerNav.findABranch.label}</a>
+              {/* <Cta buttonText="Order Pickup" href="#" style="primary-cta"></Cta>
+            <button
+              buttonText="Start My Order"
+              href="https://www.papajohns.com.mx/menu"
+              style="primary-cta"
+            >"sdgsdgg"</button> */}
+            </div>
+          </nav>
+
+          <div className="headerMainDiv py-3">
+            <div className="flex justify-between items-center">
+              <img className="h-10" src={c_headerMain.headerLogo.url} />
+            </div>
+            <div className="">
+              <button className="border border-[#6A8B2B] py-2 px-4 mr-3">
+                {" "}
+                {c_bookASlotCta.label}
+              </button>
+              <button className="border border-[#6A8B2B] py-2 px-4">
+                {" "}
+                {c_myTrolleyCta.label}
+              </button>
+            </div>
+          </div>
+          <div className="py-[6px] flex items-center">
+            <div className="btnforheader">
+              <button className="">{c_headerMain.headerBrowseCta.label}</button>
+            </div>
+            <div className="border-[black] h-10 ml-5">
+              <input className="searchforHeader" placeholder="Search" />
+            </div>
+            <div className="flex gap-14 right-5 absolute">
+              {c_headerMain.headerLabels.map((item: any) => {
+                return (
+                  <>
+                    <ul className="flex gap-12">
+                      <li>{item.label}</li>
+                    </ul>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
